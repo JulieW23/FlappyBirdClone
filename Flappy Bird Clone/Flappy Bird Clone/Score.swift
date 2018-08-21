4c447bb3b1d562cddd8f88dca0d70fd261b6e9a2
Julie Wang <jwang@konradgroup.com>
2018-08-21 18:02:33 -0400

    Added files

---
+++ Flappy Bird Clone/Flappy Bird Clone/Score.swift
@@ -0,0 +1,14 @@
+//
+//  Score.swift
+//  Flappy Bird Clone
+//
+//  Created by Julie Wang on 2018-08-20.
+//  Copyright © 2018 Julie Wang. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class Score: Object {
+    @objc dynamic var score = 0
+}
